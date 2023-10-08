444eb2a1dbf5b7872323f4d08ea8cad56b38ee37
soohyeon <su132489@gmail.com>
2023-10-09 05:16:04 +0900

    Design: font 적용

---
+++ src/assets/font/NanumSquareRound/index.ts
@@ -0,0 +1,33 @@
+import NANUM_SQUARE_ROUND_L_ttf from './NanumSquareRoundL.ttf';
+import NANUM_SQUARE_ROUND_L_woff from './NanumSquareRoundL.woff';
+import NANUM_SQUARE_ROUND_L_woff2 from './NanumSquareRoundL.woff2';
+import NANUM_SQUARE_ROUND_R_ttf from './NanumSquareRoundR.ttf';
+import NANUM_SQUARE_ROUND_R_woff from './NanumSquareRoundR.woff';
+import NANUM_SQUARE_ROUND_R_woff2 from './NanumSquareRoundR.woff2';
+import NANUM_SQUARE_ROUND_B_ttf from './NanumSquareRoundB.ttf';
+import NANUM_SQUARE_ROUND_B_woff from './NanumSquareRoundB.woff';
+import NANUM_SQUARE_ROUND_B_woff2 from './NanumSquareRoundB.woff2';
+import NANUM_SQUARE_ROUND_EB_ttf from './NanumSquareRoundEB.ttf';
+import NANUM_SQUARE_ROUND_EB_woff from './NanumSquareRoundEB.woff';
+import NANUM_SQUARE_ROUND_EB_woff2 from './NanumSquareRoundEB.woff2';
+
+export const L = {
+  ttf: NANUM_SQUARE_ROUND_L_ttf,
+  woff: NANUM_SQUARE_ROUND_L_woff,
+  woff2: NANUM_SQUARE_ROUND_L_woff2,
+};
+export const R = {
+  ttf: NANUM_SQUARE_ROUND_R_ttf,
+  woff: NANUM_SQUARE_ROUND_R_woff,
+  woff2: NANUM_SQUARE_ROUND_R_woff2,
+};
+export const B = {
+  ttf: NANUM_SQUARE_ROUND_B_ttf,
+  woff: NANUM_SQUARE_ROUND_B_woff,
+  woff2: NANUM_SQUARE_ROUND_B_woff2,
+};
+export const EB = {
+  ttf: NANUM_SQUARE_ROUND_EB_ttf,
+  woff: NANUM_SQUARE_ROUND_EB_woff,
+  woff2: NANUM_SQUARE_ROUND_EB_woff2,
+};
--- src/layouts/GloberStyle.ts
+++ src/layouts/GloberStyle.ts
@@ -1,11 +1,52 @@
 import { createGlobalStyle } from 'styled-components';
+import * as NANUM_SQURE_ROUND from '@/assets/font/NanumSquareRound';
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'RixYeoljeongdo_Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2102-01@1.0/RixYeoljeongdo_Regular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'NanumSquareRoundL';
+    src: url(${NANUM_SQURE_ROUND.L.woff2}) format('woff2'),
+          url(${NANUM_SQURE_ROUND.L.woff}) format('woff'),
+          url(${NANUM_SQURE_ROUND.L.ttf}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'NanumSquareRoundR';
+    src: url(${NANUM_SQURE_ROUND.R.woff2}) format('woff2'),
+          url(${NANUM_SQURE_ROUND.R.woff}) format('woff'),
+          url(${NANUM_SQURE_ROUND.R.ttf}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'NanumSquareRoundB';
+    src: url(${NANUM_SQURE_ROUND.B.woff2}) format('woff2'),
+          url(${NANUM_SQURE_ROUND.B.woff}) format('woff'),
+          url(${NANUM_SQURE_ROUND.B.ttf}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'NanumSquareRoundEB';
+    src: url(${NANUM_SQURE_ROUND.EB.woff2}) format('woff2'),
+          url(${NANUM_SQURE_ROUND.EB.woff}) format('woff'),
+          url(${NANUM_SQURE_ROUND.EB.ttf}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   :root {
     font-size: 15px;
     background-color: ${({ theme }) => theme.basicBg};
   }
   body {
+    font-family: 'NanumSquareRoundR';
     overflow-wrap: break-word;
     margin: 0;
     padding: 0;
@@ -26,11 +67,13 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   a {
+    font-family: 'NanumSquareRoundR';
     color: ${({ theme }) => theme.black333};
     text-decoration: none;
   }
 
   button {
+    font-family: 'NanumSquareRoundR';
     border: none;
   }
 
