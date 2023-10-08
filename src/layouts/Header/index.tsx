7f03bbd0b46b6583c8b5679ee41775d1e162c6aa
soohyeon <su132489@gmail.com>
2023-10-09 05:42:07 +0900

    Design: Header 퍼블리싱

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';
+import Logo from '@/assets/images/Header/logo.svg';
+import Logout from '@/assets/images/Header/ic-logout.svg';
+import * as S from './style';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    // TODO: 로그인 시 true. (지금은 임의로 true)
+    setIsLogin(true);
+  }, []);
   return (
-    <header>
-      <h1>
-        <Link to="/">KEEP ON</Link>
+    <S.Header>
+      <h1 className="site name">
+        <Link to="/">
+          KEEP ON
+          <img width={150} height={34} src={Logo} alt="logo" />
+        </Link>
       </h1>
-    </header>
+      {isLogin && (
+        <S.LogoutButton>
+          <img width={30} height={30} src={Logout} alt="" />
+        </S.LogoutButton>
+      )}
+    </S.Header>
   );
 }
---
+++ src/layouts/Header/style.ts
@@ -0,0 +1,33 @@
+import { styled } from 'styled-components';
+
+export const Header = styled.header`
+  position: relative;
+  overflow: hidden;
+  h1 {
+    font-size: 0;
+    margin: 0;
+    padding: 12px;
+  }
+`;
+
+export const LogoutButton = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+  background-color: ${({ theme }) => theme.basicGray};
+  height: 100%;
+  padding: 14px;
+  &::before {
+    content: '';
+    display: block;
+    width: 0px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: -3px;
+    border-top: 58px solid transparent;
+    border-bottom: 0px solid transparent;
+    border-left: 0px solid transparent;
+    border-right: 3px solid ${({ theme }) => theme.basicGray};
+  }
+`;
