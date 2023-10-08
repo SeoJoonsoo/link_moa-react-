2d020f79e3a7a367eb57363240638f8742bbe686
soohyeon <su132489@gmail.com>
2023-10-09 07:37:04 +0900

    Feat: 페이지 이동시 스크롤을 상단으로 이동시키도록 함

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -2,8 +2,10 @@ import Header from '../Header';
 import Footer from '../Footer';
 import { Outlet } from 'react-router';
 import * as S from './style';
+import useScrollToTop from '@/hooks/useScrollToTop';
 
 function AppLayout() {
+  useScrollToTop();
   return (
     <>
       <Header />
